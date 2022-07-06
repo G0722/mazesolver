315a49649cb3767c879f866eca7147190eccbac0
Gordon Yang <yanggordon722@gmail.com>
2022-07-06 16:16:57 -0700

    latest version test

--- mazesolution.py
+++ mazesolution.py
@@ -1,8 +1,5 @@
 import cv2
 import numpy as np
-from warnings import warn
-import heapq
-from math import sqrt
 
 class Node:
     """
@@ -23,16 +20,13 @@ class Node:
     def __repr__(self):
       return f"{self.position} - g: {self.g} h: {self.h} f: {self.f}"
 
-    # defining less than for purposes of heap queue
-    def __lt__(self, other):
-      return self.f < other.f
 
-    # defining greater than for purposes of heap queue
-    def __gt__(self, other):
-      return self.f > other.f
+def thin_maze_image(image):
+    # convert maze image into 1px-wide skeleton using the Zhang-Suen thinning algorithm
+    thn = cv2.ximgproc.thinning(image, None, thinningType=cv2.ximgproc.THINNING_ZHANGSUEN)
 
-# algorithm to find holes in skeleton and fill them with white
-def fill_thinned_holes(thn):
+    # algorithm to find holes in skeleton and fill them with white
+    # holes typically happen at junctions/turns
     holes = []
     for i in range(1,len(thn)-1):
         for j in range(1,len(thn[i])-1):
@@ -48,124 +42,97 @@ def fill_thinned_holes(thn):
         i, j = h[0], h[1]
         thn[i][j] = 255
 
-def thin_maze_image(folder,name):
-    path = folder+'/'+name
-    image = cv2.imread(path,0)
-
-    # convert maze image into 1px-wide skeleton using the Zhang-Suen thinning algorithm
-    thn = cv2.ximgproc.thinning(image, None, thinningType=cv2.ximgproc.THINNING_ZHANGSUEN)
-
-    # algorithm to find holes in skeleton and fill them with white
-    # holes typically happen at junctions
-    fill_thinned_holes(thn)
-
     # save thinned and filled image
-    cv2.imwrite(f'{folder}/thinned_{name}', thn)
-
-def return_path(current_node):
-    path = []
-    current = current_node
-    while current is not None:
-        path.append(current.position)
-        current = current.parent
-    return path[::-1]  # Return reversed path
+    # cv2.imwrite(f'{folder}/thinned_{name}', thn)
+    return thn
 
 def astar(maze, start, end):
-    """
-    Returns a list of tuples as a path from the given start to the given end in the given maze
-    :param maze:
-    :param start:
-    :param end:
-    :return:    """
-    #create start and end node
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+
+    # Create start and end node
     start_node = Node(None, start)
     start_node.g = start_node.h = start_node.f = 0
     end_node = Node(None, end)
     end_node.g = end_node.h = end_node.f = 0
 
-    # initialize both open and closed list
+    # Initialize both open and closed list
     open_list = []
     closed_list = []
 
-    # Heapify open list and add the start_node
-    heapq.heapify(open_list)
-    heapq.heappush(open_list, start_node)
+    # Add the start node
+    open_list.append(start_node)
 
-    # Add a stop condition
-    outer_iterations = 0
-    max_iterations = (len(maze[0])*len(maze)) // 2
-
-    # We may only search up, down, left and right no diagonals
-    adjacent_squares = ((0, -1), (0, 1), (-1, 0), (1, 0),)
-
-    # Loop until reach the end node
+    # Loop until you find the end
+    iters = 0
     while len(open_list) > 0:
-        outer_iterations += 1
-
-        # if we hit this condition we stop and return path as is
-        # will not contain destination
-        if outer_iterations >= max_iterations:
-            warn("Giving up on pathfinding. Too many iterations.")
-            return(return_path(current_node))
-
+        print(f"Iteration #{iters}")
+        iters += 1
         # Get the current node
-        current_node = heapq.heappop(open_list)
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
         closed_list.append(current_node)
 
         # Found the goal
         if current_node == end_node:
-            return return_path(current_node)
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
 
         # Generate children
         children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: # Adjacent squares
 
-        # Loop through all adjacent squares
-        for new_position in adjacent_squares:
-            # Get adjacent node position
-            node_position = (current_node.position[0]+new_position[0], current_node.position[1]+new_position[1])
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
 
             # Make sure within range
-            if (node_position[0] > (len(maze) - 1) or node_position[0] < 0
-                or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0):
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
                 continue
 
-            # Make sure path is white (walkable)
-            if maze[node_position[0]][node_position[1]]:
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
                 continue
 
-            # Create new adjacent node object and append to children
+            # Create new node
             new_node = Node(current_node, node_position)
+
+            # Append
             children.append(new_node)
 
-        # Loop through all children
+        # Loop through children
         for child in children:
-            # Child is on the closed list?
-            if len([closed_child for closed_child in closed_list if closed_child == child]) > 0:
-                continue
 
-            # Create f, g, h values
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
             child.g = current_node.g + 1
-
-            # Euclidean distance from child to end node
-            child.h = int(sqrt((child.position[0]-end_node.position[0])**2 + (child.position[1]-end_node.position[1])**2))
-            # Manhattan distance from child to end node
-            #child.h = abs(child.position[0]-end_node.position[0]) + abs(child.position[1]-end_node.position[1])
-            child.f = child.h + child.g     # heuristic for A* alg: f = h + g
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
 
             # Child is already in the open list
-            if len([open_node for open_node in open_list if child.position == open_node.position and child.g > open_node.g]) > 0:
-                continue
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
 
             # Add the child to the open list
-            heapq.heappush(open_list, child)
-
-    # Happens if we exceed maximum iterations allowed
-    warn("Couldn't get a path to the destination")
-    return None
+            open_list.append(child)
 
 def draw_path(maze, path):
     for p in path:
-        maze[path[0]][path[1]] = (255,0,0)
+        maze[p[0]][p[1]] = (255,0,0)
 
 def find_start_end(maze):
     # search the very edge (first/last row and first/last col) to locate start/end positions
@@ -213,3 +180,17 @@ def find_start_end(maze):
             elif key == 'right':
                 points.append((locations[key][0]-lengths[key]//2, locations[key][1]))
     return points
+
+# whole solution process.
+def solve_maze(image_path):
+    image = cv2.imread(image_path, 0)               # obtain the image from the file path
+    thn = thin_maze_image(image)                    # thin the maze image
+    cv2.imwrite("mazeimages/thin_maze.png", thn)
+    start_end = find_start_end(thn)                 # find start and end points of maze
+    print(f"start: {start_end[0]}, end: {start_end[1]}")
+    path = astar(thn, start_end[0], start_end[1])   # find solution path using A-star algorithm
+    print(path)
+    # draw_path(thn, path)                            # draw the path in red on the maze
+    # cv2.imwrite('docs/images/ZS_solved_veryhardmaze.png', thn)
+
+solve_maze('mazeimages/maze.png')
